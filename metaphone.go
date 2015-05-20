7fcf5e474d0e1f880c2ebbad4eba1db98b882fc5
Adele Dewey-Lopez <adele@seed.co>
2015-05-20 13:21:48 -0700

    finish up basic algorithm

--- metaphone.go
+++ metaphone.go
@@ -47,11 +47,48 @@ import (
 )
 
 type phoneticData struct {
-	t               string
+	word            string
 	cur             int
 	isSlavoGermanic bool
 	metaphone1      string
 	metaphone2      string
+}
+
+func (p *phoneticData) endsWith(matches ...string) bool {
+	end := strings.Index(p.word[p.cur:], " ")
+	if end != -1 {
+		for _, str := range matches {
+
+			if strings.Contains(p.word[p.cur:p.cur+end+1], str+" ") {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (p *phoneticData) beginsWith(matches ...string) bool {
+	start := strings.LastIndex(p.word[:p.cur], " ")
+	if start != -1 {
+		for _, str := range matches {
+
+			if strings.Contains(p.word[start:p.cur], " "+str) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (p *phoneticData) containsAny(matches ...string) bool {
+	for _, str := range matches {
+
+		if strings.Contains(p.word, str) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func (p *phoneticData) matchesAny(pos int, matches ...string) bool {
@@ -65,8 +102,11 @@ func (p *phoneticData) matchesAny(pos int, matches ...string) bool {
 
 	for i, str := range matches {
 		size := len(matches[i])
-		if p.t[p.cur+pos:p.cur+size+pos] == str {
-			return true
+		// bounds check
+		if p.cur+pos+size <= len(p.word) {
+			if p.word[p.cur+pos:p.cur+size+pos] == str {
+				return true
+			}
 		}
 	}
 
@@ -95,22 +135,24 @@ func (p *phoneticData) isVowel(pos int) bool {
 func (p *phoneticData) b() {
 	p.add("p")
 	// skip double b
-	if p.t[p.cur+1] == 'b' {
+	if p.matchesAny(1, "b") {
 		p.skip(1)
 	}
 }
 
 func (p *phoneticData) ç() {
 	p.add("s")
+	p.skip(1) // because this character is two bytes
 }
 
 func (p *phoneticData) c() {
-	if p.cur > 1 && !p.isVowel(-2) && p.matchesAny(-1, "ach") && !p.matchesAny(2, "i") &&
+	if !p.matchesAny(-1, " ") && !p.matchesAny(-2, " ") && !p.isVowel(-2) &&
+		p.matchesAny(-1, "ach") && !p.matchesAny(2, "i") &&
 		(!p.matchesAny(2, "e") || p.matchesAny(-2, "acher")) {
 		// various germanic
 		p.add("k")
 		p.skip(1)
-	} else if p.cur == 0 && p.matchesAny(0, "caesar") {
+	} else if p.matchesAny(-1, " caesar") {
 		// special case: "caesar"
 		p.add("s")
 		p.skip(1)
@@ -120,25 +162,29 @@ func (p *phoneticData) c() {
 		p.skip(1)
 	} else if p.matchesAny(0, "ch") {
 		// ch
-		if p.cur > 0 && p.matchesAny(0, "chae") {
+		if !p.matchesAny(-1, " ") && p.matchesAny(0, "chae") {
 			// find "michael"
 			p.add("k")
-		} else if p.cur == 0 && !p.matchesAny(0, "chore") &&
-			p.matchesAny(1, "harac", "haris", "hor", "hym", "hia", "hem") {
+		} else if !p.matchesAny(0, "chore") &&
+			p.matchesAny(-1, " charac", " charis", " chor", " chym", " chia", " chem", " chla") {
 			// greek roots
 			p.add("k")
-		} else if p.matchesAny(-p.cur, "van ", "von ", "sch") ||
+		} else if p.containsAny(" van ", " von ", " sch") || p.matchesAny(-3, "psych") ||
 			p.matchesAny(-2, "orches", "archit", "orchid") || p.matchesAny(2, "t", "s") ||
-			((p.matchesAny(-1, "a", "e", "o", "u") || p.cur == 0) &&
+			(p.matchesAny(-1, "a", "e", "o", "u", " ") &&
 				p.matchesAny(2, "l", "r", "n", "m", "b", "h", "f", "v", "w", " ")) {
 			// germanic greek or otherwise "ch" for "kh" sound
 			// "architect" but not "arch", "orchestra" or "orchid"
 			// e.g., "watchler", "wechsler", but not "tichner"
 			p.add("k")
-		} else if p.cur > 0 {
-			if p.matchesAny(-p.cur, "mc") {
+		} else if !p.matchesAny(-1, " ") {
+			if p.matchesAny(-p.cur, " mc") {
 				// e.g. "McHugh"
-				p.add("k")
+				if p.isVowel(2) {
+					p.add("kh") //*
+				} else {
+					p.add("k")
+				}
 			} else {
 				p.add("x", "k")
 			}
@@ -154,11 +200,11 @@ func (p *phoneticData) c() {
 		// e.g. "focaccia"
 		p.add("x")
 		p.skip(2)
-	} else if p.matchesAny(0, "cc") && !(p.cur == 1 && p.matchesAny(-p.cur, "m")) {
+	} else if p.matchesAny(0, "cc") && !p.matchesAny(-p.cur, " m") {
 		// double "c", but not if e.g. "McClellan"
 		if p.matchesAny(2, "i", "e", "h") && !p.matchesAny(2, "hu") {
 			// "bellocchio" but not "bacchus"
-			if (p.cur == 1 && p.matchesAny(-1, "a")) || p.matchesAny(-1, "uccee", "ucces") {
+			if p.matchesAny(-2, " a") || p.matchesAny(-1, "uccee", "ucces") {
 				// "accident" "acceed" or "success"
 				p.add("ks")
 			} else {
@@ -220,10 +266,10 @@ func (p *phoneticData) f() {
 
 func (p *phoneticData) g() {
 	if p.matchesAny(1, "h") {
-		if p.cur > 0 && !p.isVowel(-1) {
+		if !p.matchesAny(-1, " ") && !p.isVowel(-1) {
 			p.add("k")
 			p.skip(1)
-		} else if p.cur == 0 {
+		} else if p.matchesAny(-1, " ") {
 			if p.matchesAny(2, "i") {
 				p.add("j")
 			} else {
@@ -246,8 +292,10 @@ func (p *phoneticData) g() {
 			p.skip(1)
 		}
 	} else if p.matchesAny(1, "n") {
-		if p.cur == 1 && p.isVowel(-p.cur) && !p.isSlavoGermanic {
+		if p.matchesAny(-2, " ") && p.isVowel(-1) && !p.isSlavoGermanic {
 			p.add("kn", "n")
+		} else if p.matchesAny(-1, " ") {
+			p.add("n")
 		} else {
 			// not e.g., "cagney"
 			if !p.matchesAny(2, "ey") && !p.matchesAny(1, "y") && !p.isSlavoGermanic {
@@ -261,8 +309,8 @@ func (p *phoneticData) g() {
 		// tagliaro
 		p.add("kl", "l")
 		p.skip(1)
-	} else if p.cur == 0 && p.matchesAny(1, "y") ||
-		p.matchesAny(1, "es", "ep", "eb", "el", "ey", "ib", "il", "in", "ie", "ei") {
+	} else if p.matchesAny(1, " gy") ||
+		p.matchesAny(1, "es", "ep", "eb", "el", "ey", "ib", "il", "in", "ie", "ei", "er") {
 		p.add("k", "j")
 		p.skip(1)
 	} else if p.matchesAny(1, "er", "y") &&
@@ -273,7 +321,7 @@ func (p *phoneticData) g() {
 		p.skip(1)
 	} else if p.matchesAny(1, "e", "i", "y") || p.matchesAny(-1, "aggi", "oggi") {
 		// italian e.g. "biaggi"
-		if p.matchesAny(-p.cur, "van ", "von ", "sch") || p.matchesAny(1, "et") {
+		if p.containsAny(" van ", " von ", " sch") || p.matchesAny(1, "et") {
 			// obvious germanic
 			p.add("k")
 		} else if p.matchesAny(1, "ier") {
@@ -292,7 +340,7 @@ func (p *phoneticData) g() {
 }
 
 func (p *phoneticData) h() {
-	if (p.cur == 0 || p.isVowel(-1)) && p.isVowel(1) {
+	if (p.matchesAny(-1, " ") || p.isVowel(-1)) && p.isVowel(1) {
 		// only keep if first and before vowel, or between two vowels
 		p.add("h")
 		p.skip(1)
@@ -301,26 +349,253 @@ func (p *phoneticData) h() {
 }
 
 func (p *phoneticData) j() {
-	if p.matchesAny(0, "jose") || p.matchesAny(-p.cur, "san ") {
+	if p.matchesAny(0, "jose") || p.containsAny(" san ") {
 		// obvious spanish e.g. "jose" "san jacinto"
-		if p.cur == 0 && p.matchesAny(4, " ") || p.matchesAny(-p.cur, "san ") {
+		if (p.matchesAny(-1, " ") && p.matchesAny(4, " ")) || p.containsAny(" san ") {
 			p.add("h")
 		} else {
 			p.add("j", "h")
 		}
-	} else if p.cur == 0 {
+	} else if p.matchesAny(-1, " ") {
 		p.add("j", "a")
-	} else if p.isVowel(-1) && !p.isSlavoGermanic && p.matchesAny(1, "A", "O") {
+	} else if p.isVowel(-1) && !p.isSlavoGermanic && p.matchesAny(1, "a", "o") {
 		p.add("j", "h")
 	} else if p.matchesAny(1, " ") {
 		// end of the word because of padding
-		p.add("j", " ") // ? why the space
+		p.add("j", "")
 	} else if !p.matchesAny(1, "l", "t", "k", "s", "n", "m", "b", "z") &&
 		!p.matchesAny(-1, "s", "k", "l") {
 		p.add("j")
 	}
 
 	if p.matchesAny(1, "j") {
+		p.skip(1)
+	}
+}
+
+func (p *phoneticData) k() {
+	if !p.matchesAny(-1, " kn") {
+		p.add("k")
+	}
+	if p.matchesAny(1, "k") {
+		p.skip(1)
+	}
+}
+
+func (p *phoneticData) l() {
+	if p.matchesAny(1, "l") {
+		if p.matchesAny(-1, "illo ", "illa ", "alle ") ||
+			p.endsWith("as", "os", "a", "o") &&
+				p.matchesAny(-1, "alle") {
+			p.add("l", "")
+		} else {
+			p.add("l")
+		}
+		p.skip(1)
+	} else if !p.matchesAny(-2, "colm", "coln") { //*
+		// "malcolm", "lincoln"
+		p.add("l")
+	}
+}
+
+func (p *phoneticData) m() {
+	if p.matchesAny(-1, "umb") && (p.matchesAny(2, " ") || p.matchesAny(2, "er")) {
+		p.add("m", "mp")
+		p.skip(1)
+	} else if p.matchesAny(0, "mn ") { //*
+		p.skip(1)
+	} else if !p.matchesAny(-1, " mn") { //*
+		p.add("m")
+		if p.matchesAny(1, "m") {
+			p.skip(1)
+		}
+	}
+}
+
+func (p *phoneticData) n() {
+	p.add("n")
+	if p.matchesAny(1, "n") {
+		p.skip(1)
+	}
+}
+
+func (p *phoneticData) ñ() {
+	p.add("n")
+	p.skip(1) // because this char is two bytes
+}
+
+func (p *phoneticData) p() {
+	if p.matchesAny(0, "ph") {
+		if p.matchesAny(-p.cur, " uph", " sheph", " haph") {
+			// "shepherd" "uphill" "haphazard"
+			p.add("p")
+		} else {
+			p.add("f")
+			p.skip(1)
+		}
+	} else if p.matchesAny(1, "b", "p") {
+		p.add("p")
+		p.skip(1)
+	} else if !p.matchesAny(-1, " pn", " ps") {
+		p.add("p")
+	}
+}
+
+func (p *phoneticData) q() {
+	if p.matchesAny(1, "q") {
+		p.skip(1)
+	}
+	p.add("k")
+}
+
+func (p *phoneticData) r() {
+	if p.matchesAny(0, "r ") && !p.isSlavoGermanic &&
+		p.matchesAny(-2, "ie") && !p.matchesAny(-4, "me", "ma") {
+		p.add("", "r")
+	} else {
+		p.add("r")
+	}
+
+	if p.matchesAny(1, "r") {
+		p.skip(1)
+	}
+}
+
+func (p *phoneticData) s() {
+	if p.matchesAny(-1, "isl", "ysl") {
+		// special cases: "island" "carlysle"
+		// skip it
+	} else if p.matchesAny(-1, " sugar") {
+		p.add("x", "s")
+	} else if p.matchesAny(0, "sh") {
+		if p.matchesAny(1, "holm", "holz", "heim", "hoek") {
+			p.add("s")
+		} else {
+			p.add("x")
+		}
+		p.skip(1)
+	} else if p.matchesAny(0, "sio", "sia") {
+		if !p.isSlavoGermanic {
+			p.add("s", "x")
+		} else {
+			p.add("s")
+		}
+		p.skip(2)
+	} else if p.matchesAny(-1, " sm", " sn", " sl", " sw") || p.matchesAny(1, "z") {
+		p.add("s", "x")
+		if p.matchesAny(1, "z") {
+			p.skip(1)
+		}
+	} else if p.matchesAny(0, "sc") {
+		// Schlesinger's rule
+		if p.matchesAny(2, "h") {
+			if p.matchesAny(3, "oo", "er", "en", "uy", "ed", "em") {
+				// dutch origin
+				if p.matchesAny(3, "er", "en") {
+					p.add("x", "sk")
+				} else {
+					p.add("sk")
+				}
+
+			} else if p.matchesAny(-1, " ") && !p.isVowel(3) && !p.matchesAny(3, "w") {
+				p.add("x", "s")
+			} else {
+				p.add("x")
+			}
+			p.skip(2)
+		} else if p.matchesAny(2, "i", "e", "y") {
+			p.add("s")
+			p.skip(2)
+		} else {
+			p.add("sk")
+			p.skip(1) // *
+		}
+	} else if p.matchesAny(-2, "ais ", "ois ", "uis ") { // *
+		p.add("", "s")
+	} else {
+		p.add("s")
+		if p.matchesAny(1, "s", "z") {
+			p.skip(1)
+		}
+	}
+}
+
+func (p *phoneticData) t() {
+	if p.matchesAny(0, "tion") {
+		p.add("x")
+		p.skip(2)
+	} else if p.matchesAny(0, "tia", "tch") {
+		p.add("x")
+		p.skip(2)
+	} else if p.matchesAny(0, "th", "tth") {
+		if p.matchesAny(2, "om", "am") || p.containsAny(" van ", " von ", " sch") {
+			p.add("t")
+		} else {
+			p.add("0", "t")
+		}
+		p.skip(1)
+	} else {
+		p.add("t")
+		if p.matchesAny(1, "t", "d") {
+			p.skip(1)
+		}
+	}
+}
+
+func (p *phoneticData) v() {
+	if p.matchesAny(-1, " ") { // *
+		p.add("f", "p")
+	} else {
+		p.add("f")
+	}
+	if p.matchesAny(1, "v") {
+		p.skip(1)
+	}
+}
+
+func (p *phoneticData) w() {
+	if p.matchesAny(0, "wr") {
+		p.add("r")
+		p.skip(1)
+	} else if p.matchesAny(-1, " ") && p.isVowel(1) || p.matchesAny(0, "wh") {
+		if p.isVowel(1) {
+			p.add("a", "f")
+		} else {
+			p.add("a")
+		}
+	} else if p.matchesAny(1, " ") && p.isVowel(-1) || p.matchesAny(-p.cur, " sch") ||
+		p.matchesAny(-1, "ewski", "owski", "ewsky", "owsky") {
+		p.add("", "f")
+	} else if p.matchesAny(0, "witz", "wicz") {
+		p.add("ts", "fx")
+		p.skip(3)
+	}
+	// otherwise do nothing
+}
+
+func (p *phoneticData) x() {
+	if p.matchesAny(-1, " ") {
+		p.add("s")
+	} else if !(p.matchesAny(-2, "aux ", "oux ")) { // *
+		p.add("ks")
+	}
+
+	if p.matchesAny(1, "x", "ce", "ci", "cy") {
+		p.skip(1)
+	}
+}
+
+func (p *phoneticData) z() {
+	if p.matchesAny(1, "h") {
+		p.add("j")
+		p.skip(1)
+	} else if p.matchesAny(1, "zo", "zi", "za") || (p.isSlavoGermanic && !p.matchesAny(-1, "t")) {
+		p.add("s", "ts")
+	} else {
+		p.add("s")
+	}
+
+	if p.matchesAny(1, "z") {
 		p.skip(1)
 	}
 }
@@ -333,47 +608,88 @@ func Metaphone(s string) (string, string) {
 
 	// pad string
 	// normalize
-	p.t = s + "     "
-	p.t = strings.ToLower(p.t)
-
-	if p.matchesAny(0, "gn", "kn", "pn", "wr", "ps") {
-		p.skip(2)
-	}
+	p.word = " " + s + " "
+	p.word = strings.ToLower(p.word)
 
 	if p.matchesAny(0, "x") {
 		p.add("s")
+		p.skip(1)
 	}
 
-	for p.cur < len(p.t) {
-		next := p.t[p.cur]
-		//fmt.Println(p.cur, ": ", string(next))
-		switch next {
-		case 'a', 'e', 'i', 'o', 'u', 'y':
-			if p.cur == 0 {
-				p.add("a")
+	for i, next := range p.word {
+		// want to use range loop to handle unicode characters
+		// but also need to skip some characters
+		if p.cur == i {
+			//	fmt.Println(p.cur, ": ", string(next))
+			switch next {
+			case 'a', 'e', 'i', 'o', 'u', 'y':
+				if p.matchesAny(-1, " ") {
+					p.add("a")
+				}
+			case 'b':
+				p.b()
+			case 'ç':
+				p.ç()
+			case 'c':
+				p.c()
+			case 'd':
+				p.d()
+			case 'f':
+				p.f()
+			case 'g':
+				p.g()
+			case 'h':
+				p.h()
+			case 'j':
+				p.j()
+			case 'k':
+				p.k()
+			case 'l':
+				p.l()
+			case 'm':
+				p.m()
+			case 'n':
+				p.n()
+			case 'ñ':
+				p.ñ()
+			case 'p':
+				p.p()
+			case 'q':
+				p.q()
+			case 'r':
+				p.r()
+			case 's':
+				p.s()
+			case 't':
+				p.t()
+			case 'v':
+				p.v()
+			case 'w':
+				p.w()
+			case 'x':
+				p.x()
+			case 'z':
+				p.z()
 			}
-		case 'b':
-			p.b()
-		case 'ç':
-			p.ç()
-		case 'c':
-			p.c()
-		case 'd':
-			p.d()
-		case 'f':
-			p.f()
-		case 'g':
-			p.g()
-		case 'h':
-			p.h()
-		case 'j':
-			p.j()
+			p.cur++
 		}
-		p.cur++
-
 	}
-
-	fmt.Println("First: ", p.metaphone1, "\tSecond: ", p.metaphone2, "\t Original: ", s)
 
 	return p.metaphone1, p.metaphone2
+}
+
+func printMetaphonePair(s string) {
+	// just for testing
+	p1, p2 := Metaphone(s)
+	// stupid hacks so they all line up nicely
+	p2 = "\tSecond: " + p2
+	s = "\tOriginal: " + s
+	if len(p2) < 16 {
+		s = "\t" + s
+	}
+	if len(p1) < 7 {
+		p2 = "\t" + p2
+	}
+
+	fmt.Println("First: ", p1, p2, s)
 }
